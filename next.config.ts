14d81125a70a08b657204357b3e1b18ed1810cab
pooja.rathour <pooja.rathour@galaxyweblinks.in>
2025-04-14 22:59:50 +0530

    portfolio project

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ['i.ibb.co', 'pbs.twimg.com'], // Add allowed image domains here
+  },
+};
+
+export default nextConfig;
